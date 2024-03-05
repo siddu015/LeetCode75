02603a56dac227cf6d56829025224a7d050f844a
siddu015 <116783967+siddu015@users.noreply.github.com>
2024-03-05 22:18:56 +0530

    Done!

--- src/Vowels.java
+++ src/Vowels.java
@@ -6,22 +6,19 @@ public class Vowels {
         String vowels = "aeiouAEIOU";
 
         while (start < end) {
-            // Move start pointer until it points to a vowel
             while (start < end && vowels.indexOf(word[start]) == -1) {
                 start++;
             }
 
-            // Move end pointer until it points to a vowel
             while (start < end && vowels.indexOf(word[end]) == -1) {
                 end--;
             }
 
-            // Swap the vowels
             char temp = word[start];
             word[start] = word[end];
             word[end] = temp;
 
-            // Move the pointers towards each other
+
             start++;
             end--;
         }
