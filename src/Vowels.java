19f2d26d57ad4228351f7a805ed3546abb9cdd06
siddu015 <116783967+siddu015@users.noreply.github.com>
2024-03-07 00:21:12 +0530

    Done!

--- src/Vowels.java
+++ src/Vowels.java
@@ -1,31 +1,28 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Vowels {
-    public String reverseVowels(String s) {
-
-        int length = s.length();
-
-        for(int i = 0; i < length ; i++){
-            result.append(s.charAt(i));
-        }
-
-
-        int n = 0,vowel = 0;
-        for(int i = 0; i < length; i++){
-
-            if(checkVowel(result.charAt(i)) && n == 0){
-                vowel = i;
-                n = 1;
-            }
-
-            if(checkVowel(s.charAt(i)) && n == 1) {
-
-            }
-        }
-
-    }
+//    public String reverseVowels(String s) {
+//
+//        int length = s.length();
+//
+//        for(int i = 0; i < length ; i++){
+//            result.append(s.charAt(i));
+//        }
+//
+//
+//        int n = 0,vowel = 0;
+//        for(int i = 0; i < length; i++){
+//
+//            if(checkVowel(result.charAt(i)) && n == 0){
+//                vowel = i;
+//                n = 1;
+//            }
+//
+//            if(checkVowel(s.charAt(i)) && n == 1) {
+//
+//            }
+//        }
+//
+//    }
 
     private boolean checkVowel(char ch) {
         return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
