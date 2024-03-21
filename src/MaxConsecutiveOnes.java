212e2500f615c0598b1e41be42b54a0b848043a2
siddu015 <116783967+siddu015@users.noreply.github.com>
2024-03-21 15:03:56 +0530

    Done!

--- src/MaxConsecutiveOnes.java
+++ src/MaxConsecutiveOnes.java
@@ -1,21 +1,19 @@
-class Solution {
+class MaxConsecutiveOnes {
     public int longestOnes(int[] nums, int k) {
-        int start=0;
-        int end=0;
-        int zeros=0;
+       int start = 0, end = 0, zeros = 0;
 
-        while(end<nums.length){
-            if(nums[end] == 0){
-                zeros++;
-            }
-            end++;
-            if(zeros>k){
-                if(nums[start] == 0){
-                    zeros--;
-                }
-                start++;
-            }
-        }
-        return end-start;
+       while(end < nums.length) {
+           if(nums[end] == 0){
+               zeros++;
+           }
+           end++;
+           if(zeros > k) {
+               if(nums[start] == 0) {
+                   zeros--;
+               }
+               start++;
+           }
+       }
+       return  end - start;
     }
 }
